75dda5877e0da6ce4b02724419b9ce6c4e2c3932
Haxu <nguyenngocminh190901@gmail.com>
2022-07-13 11:59:06 +0700

    [MVC] Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const { engine } = require("express-handlebars");
-
 const app = express();
 const port = 3000;
+
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, "public")));
 // Apply middle ware
@@ -25,21 +26,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "assets/views"));
 
-app.get("/", (req, res) => {
-    res.render("home");
-});
-
-app.get("/news", (req, res) => {
-    res.render("news");
-});
-
-app.get("/search", (req, res) => {
-    res.render("search");
-});
-
-app.post("/search", (req, res) => {
-    res.send("");
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
