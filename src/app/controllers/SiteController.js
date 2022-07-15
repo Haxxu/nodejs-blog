658e8470fe6794c9cb2df0d455bfcebb992d8644
Haxu <nguyenngocminh190901@gmail.com>
2022-07-15 10:26:59 +0700

    [CRUD] Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,16 +1,16 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     // [GET] /
-    index(req, res) {
-
-        Course.find({}, function (err, courses) {  
-            if (!err) {
-                res.json(courses);
-            } else {
-                res.status(400).json({ error: 'Error!!' });
-            }
-        });
+    index(req, res, next) {
+        Course.find({})
+            .then((courses) => {
+                res.render('home', {
+                    courses: multipleMongooseToObject(courses),
+                });
+            })
+            .catch((error) => next(error)); // .catch(next)
 
         // res.render('home');
     }
